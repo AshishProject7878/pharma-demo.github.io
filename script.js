b292bd7595a7ac776de436471eca2b8f1e5dfdf2
AshishProject7878 <ashishproject78782@gmail.com>
2025-07-07 08:33:51 +0530

    Update script49921.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
       });
 
       // Ripple effect
-      $(function () { 
+      $(function () {
         $(".nav-left a")
           .on("mouseenter", function (e) {
             const parentOffset = $(this).offset();
@@ -142,3 +142,22 @@ const openBtn = document.getElementById("open-modal");
       }, 400);
     }
   });
+
+  // --------------- Slider -------------------
+
+  let currentIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove('active');
+      if (i === index) slide.classList.add('active');
+    });
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  }
+
+  setInterval(nextSlide, 4000); // Change slide every 5 seconds
